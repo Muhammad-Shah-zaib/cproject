7f3c035baeada145ec2a081ed14bcb9adcc2adb6
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2023-12-19 19:56:12 +0500

    updated temp.c

--- temp.c
+++ temp.c
@@ -6,11 +6,11 @@
 #include "clearScreen.h"
 
 // Define color codes
-#define RED "\x1b[31m"
-#define GREEN "\x1b[32m"
-#define YELLOW "\x1b[33m"
-#define BLUE "\x1b[34m"
-#define RESET "\x1b[0m"
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define RESET "\033[1;0m"
 Car_Rental generate_empty_car( void ) {
     Car_Rental emptyCar;
 
