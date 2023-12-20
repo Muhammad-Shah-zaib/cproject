2d5eb5df9f377d84eafef1e7206a448c93add44f
Muhammd-Shah-zaib <muhammdshahzaib@outlook.com>
2023-12-20 18:16:17 +0500

    updated client_intial_prompt.h

--- client_initial_prompt.h
+++ client_initial_prompt.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -95,46 +94,12 @@ void client_initial_prompt( void ) {
                 puts("\tWrong Input");
 
                 getchar(); // clearing input buffer
-=======
-$include <stdio.h>
 
-
-void client_initial_prompt()
-{
-    
-    char choice = -1, location[30];
-    puts("\n\tWelcome"); // WELCOME PROMPT
-    do
-    {
-        printf("Please enter your destination:\n");
-        scanf("%29[^\n]", location); // ! Taking location as input with spaces allowed
-        while (getchar() != '\n')
-            ;
-        if (!(check_name(location))) // Matching different variations of spellings of the location
-        {
-            clear_screen(); // clearing screen 
-            puts("\tInvalid Location");
-            continue; // iterating again for correct input
-        }
-        while (choice < 0 || choice > 3)
-        {
-            printf("What are you looking for:\n" 
-                   "1.\tHotel Bookings\n"
-                   "2.\tCar Rental\n"
-                   "3.\tBoth\n\n"
-                   "4.\tPackages\n"
-                   "\t0 to Exit\n");
-            if (scanf("%d", &choice) != 1) // checking for valid int input
-            {
-                clear_screen();
-                puts("\tWrong Input");
-                while (getchar() != '\n'); // clearing input buffer
->>>>>>> origin/Shahzaib
 
                 continue;
             }
         }
-<<<<<<< HEAD
+
 
             switch(choice){
 
@@ -159,23 +124,4 @@ void client_initial_prompt()
     } while (choice != '0'); // ! condition for the while loop (0 to exit)
 
 }
-=======
-            switch(choice){
-                case 1: hotel_read_client(location);
-                    break;
-                case 2: car_read_client(location);
-                    break;
-                case 3: hotel_read_client(location);
-                        car_read_client(location);
-                    break;
-                // case 4: display_package_client();
-                    // break;                    
-                case 0: exit(1); // ! EXIT POINT 1
-                    break;
-            }
 
-        while (getchar() != '\n'); // clearing input buffer
-
-        choice = -1; 
-    } while (choice != 0); // ! condition for the while loop (0 to exit)
->>>>>>> origin/Shahzaib
